9c1517ac55fd0c8c853e4d33fd1c0e10b25751b5
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-03-15 11:11:44 -0400

    add check for headers

--- cypress/integration/ui-spec.js
+++ cypress/integration/ui-spec.js
@@ -18,8 +18,18 @@ describe('TodoMVC app', () => {
     cy.route('POST', '/todos').as('post')
     cy.visit('/')
     cy.get('.new-todo').type('Use schemas{enter}')
-    cy.wait('@post')
+
+    // check response passes schema
+    cy.get('@post')
       .its('response.body')
       .then(api.assertSchema('PostTodoResponse', '1.0.0'))
+
+    // check response has headers with schema name and version
+    cy.get('@post')
+      .its('response.headers')
+      .should(headers => {
+        expect(headers).to.have.property('x-schema-name', 'PostTodoResponse')
+        expect(headers).to.have.property('x-schema-version', '1.0.0')
+      })
   })
 })
