e9c625445b6005f21437ba5bd7132bd7d55a39b6
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-03-24 22:03:09 -0400

    tweaking

--- cypress/integration/fixture-spec.js
+++ cypress/integration/fixture-spec.js
@@ -9,6 +9,9 @@ Cypress.Commands.add(
   'fixtureSchema',
   (fixtureName, schemaName, schemaVersion) => {
     expect(fixtureName, 'fixture name').to.be.a('string')
+    expect(schemaName, 'schema name').to.be.a('string')
+    expect(schemaVersion, 'schema version').to.match(/^\d+\.\d+\.\d+$/)
+
     cy.fixture(fixtureName, { log: false }).then(
       api.assertSchema(schemaName, schemaVersion)
     )
